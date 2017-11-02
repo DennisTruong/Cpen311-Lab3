12e333bf2d55067048b399f8ac81dbc08bfcc79f
Redwan Mahmud <redwan.mahmud07@gmail.com>
2017-11-01 22:28:32 -0700

    fixing the bug where song starts playing very fast

--- control_speed.sv
+++ control_speed.sv
@@ -1,21 +1,22 @@
-module control_speed(clock, dec_speed, inc_speed, defaultSpeed, speed_control);
+module control_speed(clock, dec_speed, inc_speed, defaultSpeed, speed_control, pressed);
 	
 
 	input logic clock, dec_speed, inc_speed, defaultSpeed;
+	input logic [7:0] pressed;
 	output logic [15:0] speed_control;
 
 	always_ff @(posedge clock) begin
-		if(defaultSpeed)
+		if(defaultSpeed & !inc_speed & !dec_speed)
 				speed_control <= 1136;     // no need to adjust speed for def
 
 		else if(inc_speed)
-				speed_control <= 1136 + 5;  //addin
+				speed_control <= speed_control + 50;  //addin
 
 		else if (dec_speed)
-				speed_control <= 1136 - 5; // deducting 
+				speed_control <= speed_control - 50; // deducting 
 			
-		else 
-				speed_control <= speed_control;
+		else if(pressed == 8'h45)
+				speed_control <= 1136;
 		end 
 
 endmodule
